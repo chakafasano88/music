a83f42127ab1b6f1d82d854ef1abdf6bafe73e56
Matt <matt.fasano88@gmail>
2017-04-19 18:21:53 -0400

    Update2

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
 require "sinatra"
 require "sinatra/activerecord"
 require "./models"
-# #this is needed for sinatra flash
-# require "bundler/setup"
-# require "sinatra/flash"
-# require "sinatra/flash"
+#this is needed for sinatra flash
+require "bundler/setup"
+require "sinatra/flash"
+require "sinatra/flash"
 
 set :database, "sqlite3:HQ.sqlite3"
 
@@ -33,5 +33,17 @@ erb :posts
 end
 
 get "/user" do
-  
+  @users = Users.all
+end
+
+get "/user_create" do
+    Users.create(user_name:"LeiMafia", password:"goaway92",  fname:"Leila", lname:"Mafoud")
+end
+
+get "/posts" do
+  @posts = Posts.all
+end
+
+get "/post_create" do
+  Posts.create(title: "Bowery Electric", date: "1/21/2017", user_id: "12345" )
 end
