f61141afd7989aa4b638c3af5987b6f84149baa0
Matt <matt.fasano88@gmail>
2017-04-20 14:29:43 -0400

    Update5

--- app.rb
+++ app.rb
@@ -9,8 +9,6 @@ require "sinatra/flash"
 set :database, "sqlite3:HQ.sqlite3"
 set :sessions, true
 
-
-
 get "/" do
 "Hello World"
 erb :home
@@ -40,30 +38,42 @@ end
 get "/sign-in" do
   erb :sign_in_form
 end
+# Allows user to sign in
+post '/sign-in' do
+  @user = User.where(user_name: params[:user_name]).first
+  if @user && @user.password == params[:password]
+    session[:user_id] = @user.id
+    flash[:notice] = "You've been signed in successfully"
+    redirect '/'
+  else
+    flash[:notice] = "There was an issue signing you in"
+   end
+   redirect "/"
+ end
 
 get "/sign-out" do
   session[:user_id] = nil
   redirect "/"
 end
 
+get "/sign-up" do
+  erb :sign_up
+end
+
 get "/login-failed" do
   erb :login_failed
 end
-
-def current_user
-  if session [:user_id]
-    @current_user = User.find(session[:user_id])
-  end
+# This is the profile
+get "/profile" do
+  @user = User.find(session[:user_id])
+  erb :profile
 end
 
-post '/sign-in' do
-  @user = User.where(user_name: params[:user_name]).first
-  if @user && @user.password == params[:password]
-    redirect '/'
-  else
-    redirect '/login-failed'
-   end
- end
+def current_user
+  if session[:user_id]
+    User.find(session[:user_id])
+  end
+end
 
 # post '/sign-in' do
 #   puts params[:user_name]
@@ -81,4 +91,18 @@ end
 
 get "/post_create" do
   Post.create(title: "Bowery Electric", date: "1/21/2017", user_id: "12345")
+end
+
+post '/new_account' do
+  @user = User.create(params[:user])
+  session[:user_id] = @user.id
+  redirect '/'
+end
+
+get '/new_account' do
+  erb :new_account
+end
+
+get 'user-posts' do
+  erb :user_posts
 end
